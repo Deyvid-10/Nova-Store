49de76fcdd760315d7c3c0d0d62aff70a4d594e5
Deyvid-10 <marmolejodeyvid@gmail.com>
2026-01-05 17:18:40 -0400

    .

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -35,7 +35,7 @@ export default function ProductList({queries}){
     <div className="bg-white flex-wrap my-5">
       
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product, index) => (
+          {products.sort(() => Math.random() - 0.5).map((product, index) => (
               
               <Item key={index} product={product}/>
           ))}
