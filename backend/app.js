a714c713de9ef941af70adad3fc3a2f11d9f1fe6
Deyvid-10 <marmolejodeyvid@gmail.com>
2026-01-05 03:01:59 -0400

    .

--- backend/app.js
+++ backend/app.js
@@ -11,18 +11,14 @@ app.use(express.json())
 app.use(static_('public'));
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:4173",
-  "https://nova-store-10.netlify.app/"
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:4173",
+//   "https://nova-store-10.netlify.app/"
+// ];
 
 const corsOptions = {
-    origin: (origin, callback) =>{
-        if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    }
-    },
+    origin: "*",
     methods: ["GET", "POST", "DELETE", "PUT"],
     // allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
